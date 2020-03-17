e43abec6cc5b11778a22a1580c94f7523debec4b
Thomas H. Li <34053587+thomas-hai-li@users.noreply.github.com>
2020-03-17 04:34:08 -0400

    table styl

--- admin/admin_control.js
+++ admin/admin_control.js
@@ -1,8 +1,8 @@
 function sendQ() {
     var baseurl = "https://best-airbnb.herokuapp.com";
     var sqlQ = document.getElementById("sqlq").value;
-    sqlQ = sqlQ.replace(/\n/g," ");
-    if (sqlQ.length > 0){
+    sqlQ = sqlQ.replace(/\n/g, " ");
+    if (sqlQ.length > 0) {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", baseurl + "/custom?sql=" + sqlQ, true);
         xhr.onreadystatechange = function () {
@@ -12,31 +12,31 @@ function sendQ() {
 
                 //main table content we fill from data from the rest call
                 var main = "";
-                for( let attr in results[0]){
-                    main+= "<th> "+attr+" </th>"
+                for (let attr in results[0]) {
+                    main += "<th> " + attr + " </th>"
                 }
                 for (i = 0; i < results.length; i++) {
                     main += "<tr>"
-                    for( let attr in results[0]){
-                        main+= "<th> "+results[i][attr]+" </th>"
+                    for (let attr in results[0]) {
+                        main += "<td> " + results[i][attr] + " </td>"
                     }
                     main += "</tr>";
                 }
-                var tbl = "<table>"+main + "</table>";
+                var tbl = "<table class='table'>" + main + "</table>";
                 document.getElementById("personinfo").innerHTML = tbl;
-            } else if(xhr.status === 400){
-                alert("Invalid SQL Request") ;
+            } else if (xhr.status === 400) {
+                alert("Invalid SQL Request");
                 document.getElementById("sqlq").value = "";
                 return false;
             }
         };
         xhr.send();
-    }else{
-        alert("SQL Command Cannot Be Empty") ;
+    } else {
+        alert("SQL Command Cannot Be Empty");
     }
-  
+
 }
 
-function clear(){
+function clear() {
     document.getElementById("personinfo").innerHTML = " ";
 }
